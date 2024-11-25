bba7c677de95feaa430951484692400a656ae371
Anandreddy125 <108915667+Anandreddy125@users.noreply.github.com>
2024-11-25 16:19:20 +0530

    Update main.tf

--- private-infra/main.tf
+++ private-infra/main.tf
@@ -487,19 +487,3 @@ resource "aws_lb_target_group_attachment" "worker_attachments" {
   target_id        = aws_instance.k3s_worker[count.index].id
   port             = var.k3s_service_port
 }
-
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "anrs-k3s" # change this
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "k3s-terraform-lock"
-  hash_key       = "LockID"
-  read_capacity = 20
-  write_capacity = 20
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}

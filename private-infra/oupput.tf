4b616d17aa455ddf374496b962bdd922d64d86ce
Anandreddy125 <108915667+Anandreddy125@users.noreply.github.com>
2024-11-26 10:40:25 +0530

    Update oupput.tf

--- private-infra/oupput.tf
+++ private-infra/oupput.tf
@@ -1,44 +1,15 @@
-# Output the Elastic IP for the NAT Gateway
-output "nat_gateway_eip" {
-  value = aws_eip.net-eip.public_ip
-  description = "Elastic IP address of the NAT Gateway"
+output "bastion_public_ip_region_1" {
+  value = aws_instance.bastion_region_1.public_ip
 }
 
-# Output the Elastic IP for the second NAT Gateway
-output "nat_gateway_eip1" {
-  value = aws_eip.net-eip1.public_ip
-  description = "Elastic IP address of the second NAT Gateway"
+output "bastion_public_ip_region_2" {
+  value = aws_instance.bastion_region_2.public_ip
 }
 
-output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
-  description = "Public IP of the Bastion host"
+output "k3s_master_private_ip_region_1" {
+  value = aws_instance.k3s_master_region_1.private_ip
 }
 
-output "load_balancer_dns" {
-  value = aws_lb.my_nlb.dns_name
-  description = "DNS Name of the Load Balancer"
+output "k3s_master_private_ip_region_2" {
+  value = aws_instance.k3s_master_region_2.private_ip
 }
-
-# Output the private IP of the Bastion EC2 instance
-output "bastion_private_ip" {
-  value       = aws_instance.bastion.private_ip
-  description = "Private IP address of the Bastion EC2 instance"
-}
-
-# Output the instance ID of the Bastion EC2 instance
-output "bastion_instance_id" {
-  value       = aws_instance.bastion.id
-  description = "Instance ID of the Bastion EC2 instance"
-}
-
-# Output the private IP of the K3s master node
-output "k3s_master_private_ip" {
-  value = aws_instance.k3s_master.private_ip
-}
-
-# Output the private IPs of all K3s worker nodes
-output "k3s_worker_private_ips" {
-  value = [for instance in aws_instance.k3s_worker : instance.private_ip]
-}
-
